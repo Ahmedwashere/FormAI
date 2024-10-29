2bfe0a86995b7953f1ee7e19141333fc93a5bcf0
ahmedwashere <aeldeso@xula.edu>
2024-10-29 16:27:35 -0500

    Added App logo circle that will be used in the login and signup to SharedComponents.kt in a preview.

--- app/src/main/java/com/example/formai/MainActivity.kt
+++ app/src/main/java/com/example/formai/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.formai.ui.screens.login.LoginScreen
 import com.example.formai.ui.screens.welcome.WelcomeScreen
 import com.example.formai.ui.theme.FormAITheme
 
@@ -14,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FormAITheme {
-                LoginScreen()
+                WelcomeScreen()
             }
         }
     }
--- app/src/main/java/com/example/formai/ui/screens/SharedComponents.kt
+++ app/src/main/java/com/example/formai/ui/screens/SharedComponents.kt
@@ -1,15 +1,24 @@
 package com.example.formai.ui.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.formai.R
 
 /**
  * This file will store shared composables/components which are used across more than one screen.
@@ -42,4 +51,18 @@ fun AppButton(
     ) {
         content()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CircularAppLogoPreview() {
+    Image(
+        painter = painterResource(id = R.drawable.app_logo_no_background),
+        contentDescription = "Circle that's grey with App logo within.",
+        modifier = Modifier
+            .size(153.dp)
+            .clip(CircleShape)
+            .background(Color(0xFF444444))
+            .padding(24.dp),
+    )
 }
